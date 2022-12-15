663eee912d882d6c9d74db87479396e93a5a00ba
Salvato Luice <salvatoluice5@gmai.com>
2022-12-15 14:19:58 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Mobiles from './components/categories/Mobiles';
+import Header from './components/Header/Header';
 import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/mobiles' element={<Mobiles />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import './header.css'
+import { IoMdCall, IoMdPerson } from 'react-icons/io'
+import { FiSearch } from 'react-icons/fi'
+import { GiShoppingCart } from 'react-icons/gi'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='navbar'>
+      <div className='top-nav'>
+        <div className='nav_left'>
+          <p>Customer Care</p>
+          <p><IoMdCall /> +254 709 782 000</p>
+        </div>
+        <div className='nav_right'>
+          <p>Sell</p>
+          <p>Help & Contact</p>
+        </div>
+      </div>
+      <div className='bottom_nav'>
+        <div className='logo'>
+          <Link to='/'>
+            <img src='https://sky.garden/assets/skylogo_white.png' alt="logo" />
+          </Link>
+          <button>CATEGORIES</button>
+        </div>
+        <div className='search'>
+          <input type="text" placeholder="Search for products, shops and cateories" /> 
+          <button><FiSearch className='search_icon'/></button>
+        </div>
+        <div className='right'>
+          <div className='login'>
+            <p>Log In</p>
+            <IoMdPerson />
+          </div>
+          <div className='cart'>
+            <p>Cart</p>
+            <GiShoppingCart />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
