e5cc27d3775cb1fa1507cc618a8054cdc03bc0a3
Salvato Luice <salvatoluice5@gmai.com>
2022-12-15 12:35:02 +0300

    Initial work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Mobiles from './components/categories/Mobiles';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-  
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/mobiles' element={<Mobiles />} />
+      </Routes>
     </div>
   );
 }
--- src/components/categories/Mobiles.js
+++ src/components/categories/Mobiles.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Mobiles = () => {
   return (
-    <div>Mobiles</div>
+    <div className='app_mobiles'>
+      <div className='header_starter'>
+        <div className='text'>
+          <h1>Mobile phones & Tablets</h1>
+          <p>Buy Smartphones, Featured Phones, Tablets or Mobile Accessories at great
+            prices and top brands from the sellers around Nairobi. Same day delivery
+            for all Nairobi orders.</p>
+        </div>
+        <div className='image'>
+          <img src='https://skygardenwe.blob.core.windows.net/media/original_images/smartphones_EZ8eMfR.png' alt='' />
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://skygarden.azureedge.net/images-thumbnails/products/1314451-ab5b24bd133e46e795ce052165868eed.jpg' />
+        <h3>Blackberry Passport Battery</h3>
+        <p>Meroda Enterprices Kenya</p>
+        <h1>Ksh. 1, 499</h1>
+        <button>BUY NOW</button>
+      </div>
+
+
+    </div>
   )
 }
 
