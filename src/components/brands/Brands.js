c1fe3530a9b1b2510e71d5267dee240f0ccd362b
Salvato Luice <salvatoluice5@gmai.com>
2022-12-15 15:17:44 +0300

    Add brands

--- src/components/brands/Brands.js
+++ src/components/brands/Brands.js
@@ -3,7 +3,15 @@ import './brands.css'
 
 const Brands = () => {
   return (
-    <div>Brands</div>
+    <div className='brands'>
+      <img src='https://skygarden.azureedge.net/media/original_images/fone-express.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/Hisense.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/Hotpoint_2.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/Marini.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/phillips_1.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/lg_1.png' />
+      <img src='https://skygarden.azureedge.net/media/original_images/Silentnight.png' />
+    </div>
   )
 }
 
