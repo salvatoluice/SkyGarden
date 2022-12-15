b1d2b97fa5378909ca6de977431d870eddfc9562
Salvato Luice <salvatoluice5@gmai.com>
2022-12-15 15:01:53 +0300

    Category

---
+++ src/components/categories/Category.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { FiArrowRightCircle } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import './category.css'
+
+const Category = () => {
+  return (
+    <div className='categories'>
+    <Link to='/mobiles'>
+      <div className='category'>
+        <img src='https://skygarden.azureedge.net/media/original_images/smartphones_EZ8eMfR.png' alt='' />
+        <div className='category_details'>
+          <h4>SmartPhones</h4>
+          <FiArrowRightCircle />
+        </div>
+      </div>
+    </Link>
+    <div className='category'>
+      <img src='https://skygarden.azureedge.net/media/original_images/laptop_FDcFYvJ.png' alt='' />
+      <div className='category_details'>
+        <h4>Computing</h4>
+        <FiArrowRightCircle />
+      </div>
+    </div>
+    <div className='category'>
+      <img src='https://skygarden.azureedge.net/media/original_images/laptop_FDcFYvJ.png' alt='' />
+      <div className='category_details'>
+        <h4>Home Appliances</h4>
+        <FiArrowRightCircle />
+      </div>
+    </div>
+    <div className='category'>
+      <img src='https://skygarden.azureedge.net/media/original_images/televisions_mX7PUyv.png' alt='' />
+      <div className='category_details'>
+        <h4>Electronics</h4>
+        <FiArrowRightCircle />
+      </div>
+    </div>
+    <div className='category'>
+      <img src='https://skygarden.azureedge.net/media/original_images/Categories4.png' alt='' />
+      <div className='category_details'>
+        <h4>Furniture</h4>
+        <FiArrowRightCircle />
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Category
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,51 +1,14 @@
 import React from 'react'
-import { FiArrowRightCircle } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import Brands from '../brands/Brands'
+import Category from '../categories/Category'
 import Hero from './Hero'
 
 const Home = () => {
   return (
     <div className='app_home'>
       <Hero />
-      <div className='categories'>
-        <Link to='/mobiles'>
-          <div className='category'>
-            <img src='https://skygarden.azureedge.net/media/original_images/smartphones_EZ8eMfR.png' alt='' />
-            <div className='category_details'>
-              <h1>SmartPhones</h1>
-              <FiArrowRightCircle />
-            </div>
-          </div>
-        </Link>
-        <div className='category'>
-          <img src='https://skygarden.azureedge.net/media/original_images/laptop_FDcFYvJ.png' alt='' />
-          <div className='category_details'>
-            <h1>Computing</h1>
-            <FiArrowRightCircle />
-          </div>
-        </div>
-        <div className='category'>
-          <img src='https://skygarden.azureedge.net/media/original_images/laptop_FDcFYvJ.png' alt='' />
-          <div className='category_details'>
-            <h1>Home Appliances</h1>
-            <FiArrowRightCircle />
-          </div>
-        </div>
-        <div className='category'>
-          <img src='https://skygarden.azureedge.net/media/original_images/televisions_mX7PUyv.png' alt='' />
-          <div className='category_details'>
-            <h1>Electronics</h1>
-            <FiArrowRightCircle />
-          </div>
-        </div>
-        <div className='category'>
-          <img src='https://skygarden.azureedge.net/media/original_images/Categories4.png' alt='' />
-          <div className='category_details'>
-            <h1>Furniture</h1>
-            <FiArrowRightCircle />
-          </div>
-        </div>
-      </div>
+      <Category />
+      <Brands />
     </div>
   )
 }
