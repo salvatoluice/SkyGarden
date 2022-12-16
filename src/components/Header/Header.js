62ffc41054c7e00907815fa855da0c3780888f39
Salvato Luice <salvatoluice5@gmai.com>
2022-12-16 08:32:03 +0300

    Add user name to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './header.css'
 import { IoMdCall, IoMdPerson } from 'react-icons/io'
 import { FiSearch } from 'react-icons/fi'
 import { GiShoppingCart } from 'react-icons/gi'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
+import { auth, db, logout } from "../../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
 
 const Header = () => {
   const state = useSelector((state) => state.handleCart)
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName();
+  }, [user, loading]);
+  
   return (
     <div className='navbar'>
       <div className='top-nav'>
@@ -34,7 +59,7 @@ const Header = () => {
         <div className='right'>
           <Link to='/login'>
             <div className='logins'>
-              <p>Log In</p>
+              {!user ? <p>Log In</p> : <p className='name'>{name}</p> }
               <IoMdPerson />
             </div>
           </Link>
