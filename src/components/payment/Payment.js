79481c9c67af46d912e00787f0e80fb74a266f96
Salvato Luice <salvatoluice5@gmai.com>
2022-12-20 07:34:52 +0300

    modify checkout

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./payment.css"
 
 const Payment = () => {
   return (
     <div className='payment'>
         <div className='checkout'>
-            <h1>Sky.Garden</h1>
-            <p>Payment method coming soon</p>
+            <Link to='/'><h1>Sky.Garden</h1></Link>
+            <p>Payment method coming soon... Stay in!</p>
         </div>
     </div>
   )
